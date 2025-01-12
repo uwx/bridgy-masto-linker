41cb858b2f40a8de791afd05b44607b5f8e9ebd3
uwx <maxine@cybergal.com>
2025-01-12 13:48:35 -0300

    Add button styling

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,33 @@ function findFiberByProps(element: Element, condition: (props: Record<string, an
     return undefined;
 }
 
+function addStyle(aCss: string) {
+    const head = document.head ?? document.getElementsByTagName('head')[0];
+    if (head) {
+        const style = document.createElement('style');
+        style.setAttribute('type', 'text/css');
+        style.textContent = aCss;
+        head.appendChild(style);
+        return style;
+    }
+    return null;
+};
+
+addStyle(`
+.maxine-masto-button {
+    background: none;
+    border: none;
+    cursor: pointer;
+    width: 32px;
+    height: 32px;
+    line-height: 32px;
+}
+.maxine-masto-button:hover {
+    background: rgb(20, 27, 35);
+    border-radius: 50%;
+}
+`);
+
 // document.querySelector('[data-testid^="postThreadItem-by-"]').__reactFiber$9oo16v5l9w.return.return.memoizedProps.record.bridgyOriginalUrl
 // document.querySelector('[data-testid="feedItem-by-fedi.eblu.space"]').__reactFiber$en58sxiacu8.return.return.return.return.return.return.return.memoizedProps.record.bridgyOriginalUrl
 
@@ -38,9 +65,10 @@ setInterval(() => {
 
         const originalUrl = fiber.memoizedProps.record.bridgyOriginalUrl;
         if (!originalUrl) continue;
-        
+
         const button = document.createElement('button');
         button.textContent = '🌐';
+        button.className = 'maxine-masto-button';
         button.onclick = e => {
             e.stopPropagation();
             e.preventDefault();
@@ -49,7 +77,7 @@ setInterval(() => {
         };
 
         if (!buttons) continue;
-    
+
         buttons.append(button);
     }
 }, 250);
